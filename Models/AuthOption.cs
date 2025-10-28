f04a8b04365f477880c1ac84c260497506f98e93
Anyutaa <anyuta29032004@mail.ru>
2025-10-28 15:05:24 +0300

    Удалила ненужную функцию

--- Models/AuthOption.cs
+++ Models/AuthOption.cs
@@ -16,26 +16,5 @@ namespace Crossplatform_2_smirnova
         public static SecurityKey SigningKey =>
             new SymmetricSecurityKey(Encoding.ASCII.GetBytes("superSecretKeyMustBeLoooooongAndLonger123"));
 
-        public static string GenerateToken(int userId, string role)
-        {
-            var now = DateTime.UtcNow;
-
-            var claims = new List<Claim>
-            {
-                new Claim(ClaimsIdentity.DefaultNameClaimType, userId.ToString()),
-                new Claim(ClaimsIdentity.DefaultRoleClaimType, role)
-            };
-
-            var jwt = new JwtSecurityToken(
-                issuer: Issuer,
-                audience: Audience,
-                notBefore: now,
-                expires: now.AddHours(LifetimeInHours),
-                claims: claims,
-                signingCredentials: new SigningCredentials(SigningKey, SecurityAlgorithms.HmacSha256)
-            );
-
-            return new JwtSecurityTokenHandler().WriteToken(jwt);
-        }
     }
 }
